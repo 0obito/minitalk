e61a90b1fc44b43297b0ec6177fc2b0af5f75ff1
0obito <elmsafer.abdellah00@gmail.com>
2025-04-10 06:36:56 +0100

    Made some progress, and thought i save things just in case. The client should be good, made no tests yet though so nothing is confirmed, tomorrow inshaAllah i will finish the server and test everything.

--- client.c
+++ client.c
@@ -14,16 +14,87 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/types.h>
+
+// size_t	ft_strlen(char *str)
+// {
+// 	size_t	i;
+
+// 	if (str == NULL)
+// 		exit(1);
+// 	i = 0;
+// 	while (str[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+
+	if (str == NULL || str[0] == '\0')
+		exit(1);
+	i = 0;
+	res = 0;
+	while (str[i] == 32 || (str[i] <= 13 && str[i] >= 9))
+		i++;
+	if (!str[i])
+		exit(1);
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	while (str[i] == 32 || (str[i] <= 13 && str[i] >= 9))
+		i++;
+	if (str[i])
+		exit(1);
+	return (res);
+}
+
+int	send_char(pid_t pid, unsigned char c)
+{
+	int	i;
+	int	bit;
+
+	i = 0;
+	while (i < 8)
+	{
+		bit = c % 2;
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c /= 2;
+		i++;
+	}
+}
+
+int	send_str(pid_t pid, char *str)
+{
+	int	i;
+
+	if (str == NULL)
+		exit(1);
+	i = 0;
+	while (str[i])
+	{
+		send_char(pid, (unsigned char)str[i]);
+		i++;
+	}
+}
 
 int	main(int ac, char *av[])
 {
+	pid_t	pid;
+	char	*str;
+
 	if (ac == 3)
 	{
-		printf("Process ID received: %s\n", av[1]);
-		if (1)
-		{
-			printf("%s\n", av[2]);
-		}
+		pid = ft_atoi(av[1]);
+		str = av[2];
+		send_str(pid, str);
 	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -14,13 +14,63 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/types.h>
+
+// unsigned char	g_byte = 0;
+static unsigned char	byte = 0;
+
+void	handle_sigusr1(int signum)
+{
+	byte << 1;
+
+}
+
+void	handle_sigusr2(int signum)
+{
+	byte << 1;
+}
+
+// void	handle_signal(int signum)
+// {
+// 	if (signum == SIGUSR1)
+// 	{
+// 		printf("SIGUSR1\n");
+// 	}
+// 	else if (signum == SIGUSR2)
+// 	{
+// 		printf("SIGUSR2\n");
+// 	}
+// 	// else
+// 	// 	; //	temporarily, until I have an idea what to do here!
+// }
 
 int	main(void)
 {
-	int	pid;
+	pid_t				pid;
+	struct sigaction	act1;
+	struct sigaction	act2;
 
+	//	Get and print the current process' id:
 	pid = getpid();
 	printf("%d\n", pid);
-	kill(pid, 0);
+	//	Fill structures act1:
+	act1.sa_handler = handle_sigusr1;
+	sigemptyset(&act1.sa_mask);
+	act1.sa_flags = 0;	//there's SA_SIGINFO as well, need to look it up!
+	//	Fill structures act2:
+	act2.sa_handler = handle_sigusr2;
+	sigemptyset(&act2.sa_mask);
+	act2.sa_flags = 0;	//there's SA_SIGINFO as well, need to look it up!
+	//	handle the signal SIGUSR1:
+	if (sigaction(SIGUSR1, &act1, NULL) == -1)
+		exit(1);
+	//	handle the signal SIGUSR2:
+	if (sigaction(SIGUSR2, &act2, NULL) == -1)
+		exit(1);
+	//	waiting loop for a signal
+	while (1)
+	{
+		pause();
+	}
 	return (0);
 }
